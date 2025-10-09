6575e2d2c4e215aa8e017e7347f29d670b29832b
Mostafa Seleem <mostafa.ay.seleem@gmail.com>
2025-10-09 14:11:01 +0200

    test

--- git_workflow.bat
+++ git_workflow.bat
@@ -16,9 +16,7 @@ echo.
 :: --- Commit ---
 set /p commitMsg=Commit message: 
 if "!commitMsg!"=="" set "commitMsg=update"
-git add . ":!env!" >nul 2>&1
-git reset HEAD .env >nul 2>&1
-git reset HEAD backend_myCalorieBuddy/.env >nul 2>&1
+git add . 
 git commit -m "!commitMsg!"
 echo.
 
