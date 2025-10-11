adff06055638c6af107b0b2e6ea9d7b58e4c8dc5
Mostafa <mostafa.ay.seleem@gmail.com>
2025-10-11 15:53:39 +0200

    stable

--- start_expo.bat
+++ start_expo.bat
@@ -1,6 +1,6 @@
 @echo off
-cd /d "C:\Users\MTS\Desktop\MS_2\frontend_myCalorieBuddy"
+cd /d "C:\Users\mts\Desktop\myCalorieBuddy-wip-1.5\frontend_myCalorieBuddy"
 echo Starting Expo tunnel...
-cd /d "C:\Users\MTS\Desktop\MS_2\frontend_myCalorieBuddy"
+cd /d "C:\Users\mts\Desktop\myCalorieBuddy-wip-1.5\frontend_myCalorieBuddy"
 npx expo start -c --tunnel
 pause
